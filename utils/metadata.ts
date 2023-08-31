69c00025b9be07007c183cce76db8b923b4c625b
Dave Snider <dave.snider@gmail.com>
2023-08-31 04:54:08 -0400

    better TS version

--- utils/metadata.ts
+++ utils/metadata.ts
@@ -1,11 +1,26 @@
 // Grabs the demensions and other info of a transformed image
-export const fetchMetadata = async (metadataUrl: string) => {
+interface OriginalImageInfo {
+  file_size: number;
+  width: number;
+  height: number;
+  format: string;
+}
+
+interface ImageMetadata {
+  width: number;
+  height: number;
+  original: OriginalImageInfo;
+}
+
+// Grabs the dimensions and other info of a transformed image
+export const fetchMetadata = async (metadataUrl: string): Promise<ImageMetadata | null> => {
   try {
     const response = await fetch(metadataUrl);
     if (!response.ok) {
       throw new Error('Failed to fetch metadata');
     }
-    return await response.json();
+    const data: ImageMetadata = await response.json();
+    return data;
   } catch (error) {
     console.error('Error fetching metadata:', error);
     return null;
