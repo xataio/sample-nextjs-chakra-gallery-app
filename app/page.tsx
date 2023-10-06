9d5dc193e24486ff4f142cb23d2a0b92075c1305
Tudor Golubenco <tudor@xata.io>
2023-10-06 17:40:59 +0200

    Enable edge + no fectchMetadata

--- app/page.tsx
+++ app/page.tsx
@@ -64,40 +64,44 @@ export default async function Page({ searchParams }: { searchParams: { page: str
 
   // We use Xata's transform helper to create a thumbnail for each image
   // and apply it to the image object
+  console.time('Fetching images transforms');
   const images = compact(
-    imagesPage.records.map(async (record) => {
-      if (!record.image) {
-        return undefined;
-      }
-
-      const { url } = record.image.transform({
-        width: IMAGE_SIZE,
-        height: IMAGE_SIZE,
-        format: 'auto',
-        fit: 'cover',
-        gravity: 'top'
-      });
-
-      // Since the resulting image will be a square, we don't really need to fetch the metadata
-      // but let's do it anyway to show how it's done. Meta data provides both the original
-      // and transformed dimensions of the image.
-      // const metadata = await fetchMetadata(metadataUrl);
-
-      if (!url) {
-        return undefined;
-      }
-
-      const thumb = {
-        url,
-        attributes: {
-          width: IMAGE_SIZE, // Post transform width
-          height: IMAGE_SIZE // Post transform height
+    await Promise.all(
+      imagesPage.records.map(async (record) => {
+        if (!record.image) {
+          return undefined;
         }
-      };
 
-      return { ...record.toSerializable(), thumb };
-    })
+        const { url } = record.image.transform({
+          width: IMAGE_SIZE,
+          height: IMAGE_SIZE,
+          format: 'auto',
+          fit: 'cover',
+          gravity: 'top'
+        });
+
+        // Since the resulting image will be a square, we don't really need to fetch the metadata
+        // but let's do it anyway to show how it's done. Meta data provides both the original
+        // and transformed dimensions of the image.
+        // const metadata = await fetchMetadata(metadataUrl);
+
+        if (!url) {
+          return undefined;
+        }
+
+        const thumb = {
+          url,
+          attributes: {
+            width: IMAGE_SIZE, // Post transform width
+            height: IMAGE_SIZE // Post transform height
+          }
+        };
+
+        return { ...record.toSerializable(), thumb };
+      })
+    )
   );
+  console.timeEnd('Fetching images transforms');
 
   // Find the top 10 tags using Xata's summarize helper
   const tags = topTags.summaries.map((tagSummary) => {
