4eb07339b7fc8a17714b86fe98f32edbb11fca8c
Alexis Rico <sferadev@gmail.com>
2023-11-08 13:40:01 +0100

    Simplify code in binary upload (#17)

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -5,64 +5,41 @@ import { getXataClient } from '~/utils/xata';
 
 const xata = getXataClient();
 
-// Next.js edge runtime
-// https://nextjs.org/docs/pages/api-reference/edge
-export const runtime = 'edge';
-export const preferredRegion = 'iad1';
-
 export async function POST(request: Request) {
   // People on the internet can be mean, so let's make sure we don't allow
   // anyone to create images in the live demo
   if (process.env.READ_ONLY === 'true') {
-    return NextResponse.json(
-      { message: 'Read only mode enabled' },
-      {
-        status: 403
-      }
-    );
+    return NextResponse.json({ message: 'Read only mode enabled' }, { status: 403 });
   }
 
   // Get the form data
   const formData = await request.formData();
   const file = formData.get('file') as File;
-  const name = formData.get('name');
-  const tags = formData.get('tags');
+  const name = formData.get('name') as string;
+  const tags = formData.get('tags') as string;
 
   // Split the tags into an array from a comma separated string
-  const tagsArray = tags ? (tags as string).split(',').map((tag) => tag.trim()) : [];
-
-  const fileName: string = file.name;
-  const fileData = Buffer.from(await file.arrayBuffer());
-  const mimeType = file.type;
+  const tagsArray =
+    tags?.split(',').map((tag) => {
+      const name = tag.trim();
+      return { id: slugify(name, { lower: true }), name };
+    }) ?? [];
 
   // Create the image record in Xata
-  const record = await xata.db.image.create({
-    name: name as string,
-    image: {
-      name: fileName,
-      mediaType: mimeType,
-      // Xata expects a base64 encoded string for the file content
-      base64Content: fileData.toString('base64')
-    }
-  });
+  const record = await xata.db.image.create({ name });
+
+  // Upload the file and attach it to the image record
+  await xata.files.upload({ table: 'image', column: 'image', record: record.id }, file);
+  await xata.db.image.update(record.id, { image: { name: file.name } });
 
   // Once the image is created, create or update any related tags
-  await xata.db.tag.createOrUpdate([
-    ...tagsArray.map((tag) => ({
-      id: slugify(tag, { lower: true }),
-      name: tag
-    }))
-  ]);
+  // Also create the links between the image and the tags
+  if (tagsArray.length > 0) {
+    await xata.db.tag.createOrUpdate(tagsArray);
 
-  // Once the tags are created, create the links between the image and the tags
-  if (tagsArray.length > 0 && record.id) {
     await xata.db['tag-to-image'].create(
       // Create an array of objects with the tag id and image id
-      tagsArray.map((tag) => ({
-        id: uuid(),
-        tag: slugify(tag, { lower: true }),
-        image: record.id
-      }))
+      tagsArray.map(({ id: tag }) => ({ id: uuid(), tag, image: record.id }))
     );
   }
 
