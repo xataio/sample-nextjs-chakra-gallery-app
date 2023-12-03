64c66b34ba2e3d187560947fab27c7dbffe2f390
Tudor Golubenco <tudor@xata.io>
2023-12-03 12:33:59 +0200

    Added butterfly images as seed data

---
+++ scripts/one-click.mjs
@@ -0,0 +1,26 @@
+//@ts-check
+import dotenv from 'dotenv';
+import { exec } from 'node:child_process';
+
+dotenv.config({
+  path: '.env.local'
+});
+
+try {
+  console.log(`❯ Setting up database at ${process.env.XATA_DATABASE_URL}`);
+
+  exec(
+    `pnpm -s dlx @xata.io/cli@latest init --schema=schema.json --codegen=lib/xata.codegen.ts --db=${process.env.XATA_DATABASE_URL} --yes`,
+    (_error, stdout, stderr) => {
+      console.log('❯ Running pnpm dlx');
+
+      if (stderr) {
+        console.error(`Finished with issues: \n${stderr}`);
+        return;
+      }
+      console.log(stdout);
+    }
+  );
+} catch {
+  console.warn('Setup gone wrong.');
+}
