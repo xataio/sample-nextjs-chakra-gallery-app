fdef3823d0e0170aff49e79d8acb2a361346d0c9
Dave Snider <dave.snider@gmail.com>
2023-12-06 14:47:42 -0500

    adjust to allow profile flag on bootstrap command

--- scripts/bootstrap.mjs
+++ scripts/bootstrap.mjs
@@ -1,16 +1,24 @@
 import { execSync } from 'child_process';
 
 const args = process.argv.slice(2);
-const dbIndex = args.indexOf('--db');
-let dbValue = null;
 
-if (dbIndex !== -1 && dbIndex < args.length - 1) {
-  dbValue = args[dbIndex + 1];
+function getArgumentValue(key) {
+  const index = args.indexOf(key);
+  if (index !== -1 && index < args.length - 1) {
+    return args[index + 1];
+  }
+  return null;
 }
+
+const dbValue = getArgumentValue('--db');
+const profileValue = getArgumentValue('--profile');
 
 let cmd = 'node scripts/cleanup.mjs --force && xata init --schema schema.json --codegen=utils/xata.ts';
 if (dbValue) {
   cmd += ` --db ${dbValue}`;
+}
+if (profileValue) {
+  cmd += ` --profile ${profileValue}`;
 }
 cmd += ' && node scripts/seed.mjs';
 
