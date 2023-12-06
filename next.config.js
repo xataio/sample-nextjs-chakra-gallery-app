2caa654d5ae6c70f650dd6d234cc2236451d0f7b
Dave Snider <dave.snider@gmail.com>
2023-12-06 14:53:00 -0500

    allow staging image urls

--- next.config.js
+++ next.config.js
@@ -6,7 +6,12 @@ const nextConfig = {
       'us-west-2.storage.xata.sh',
       'eu-west-1.storage.xata.sh',
       'eu-central-1.storage.xata.sh',
-      'ap-southeast-2.storage.xata.sh'
+      'ap-southeast-2.storage.xata.sh',
+      'us-east-1.storage.staging-xata.dev',
+      'us-west-2.storage.staging-xata.dev',
+      'eu-west-1.storage.staging-xata.dev',
+      'eu-central-1.storage.staging-xata.dev',
+      'ap-southeast-2.storage.staging-xata.dev'
     ]
   }
 };
