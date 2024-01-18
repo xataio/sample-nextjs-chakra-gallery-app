05edcefafdfc16eadd1db7d817be2940d3dfccf1
Dave Snider <dave.snider@gmail.com>
2024-01-18 11:49:41 -0500

    Use new pattern for remotepatterns in config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,28 @@
+const xataDomains = [
+  'us-east-1.storage.xata.sh',
+  'us-west-2.storage.xata.sh',
+  'eu-west-1.storage.xata.sh',
+  'eu-central-1.storage.xata.sh',
+  'ap-southeast-2.storage.xata.sh',
+  'us-east-1.storage.staging-xata.dev',
+  'us-west-2.storage.staging-xata.dev',
+  'eu-west-1.storage.staging-xata.dev',
+  'eu-central-1.storage.staging-xata.dev',
+  'ap-southeast-2.storage.staging-xata.dev'
+];
+
+const xataDomainsRemote = xataDomains.map((domain) => {
+  return {
+    protocol: 'https',
+    hostname: domain,
+    port: ''
+  };
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'us-east-1.storage.xata.sh',
-      'us-west-2.storage.xata.sh',
-      'eu-west-1.storage.xata.sh',
-      'eu-central-1.storage.xata.sh',
-      'ap-southeast-2.storage.xata.sh',
-      'us-east-1.storage.staging-xata.dev',
-      'us-west-2.storage.staging-xata.dev',
-      'eu-west-1.storage.staging-xata.dev',
-      'eu-central-1.storage.staging-xata.dev',
-      'ap-southeast-2.storage.staging-xata.dev'
-    ]
+    remotePatterns: xataDomainsRemote
   }
 };
 
